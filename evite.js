80023fd415dcd8330ce129fbbc2d48f1f3db25df
thinktt <github@thinktt.e4ward.com>
2015-12-01 23:44:17 -0600

    form submit with ajax

---
+++ evite.js
@@ -0,0 +1,22 @@
+$(document).ready( function(){
+  $.fn.snow();
+  
+  // capture submit
+	$('form').submit(function() {
+    var $theForm = $(this);
+
+    // send xhr request
+    $.ajax({
+	      type: $theForm.attr('method'),
+	      url: $theForm.attr('action'),
+	      data: $theForm.serialize(),
+	      success: function(data) {
+	      console.log('Yay! Form sent.');
+      }
+    });
+
+  	// prevent submitting again
+  	 return false;
+	});
+
+});

a7b858d8e60a0036727c6471b745c75d89a73ae0
thinktt <github@thinktt.e4ward.com>
2016-11-23 23:22:54 -0600

    clean out junk

--- evite.js
+++ evite.js
@@ -1,111 +1,16 @@
-var messages = [  
- '<br>Dear friends of my humans,<br><br>' +
- 'It seems the holiday season is upon us.  My paw-sized stocking has been ' +
- 'hung <br> from the chimney with care and I’ve been yelled at on more than ' + 
- 'one occasion for <br> watering the Christmas tree...',
-
- '<br> It has been an eventful year in my territory off Laurel Grove.  Here are '+
- ' a few memories we made along the way: ',
-
- '<br>Spending a few relaxing days at the lodge in Whistler... ',
-
- '<br>Catching some rays on the pristine beaches of the Dominican Republic... ',
-
- '<br>My pack hiking the north shore of Oahu... ',
-
-  'To cap the year off right, it would be my pleasure to personally invite ' + 
-  'you to my den for a celebration on the night of December 19th.  Festivities ' +  
-  'will start at 7 PM.  My humans will provide a pony keg of the finest holiday ' +
-  'brew as well as hors d\'oeuvres.  Don\'t worry, I\'ve sampled them all and can ' +  
-  'assure you they are delicious.  Please bring any liquor and mixers you would ' + 
-  'like, as well as a chair.  Ice and cups will be provided. Also please wear ' +
-  'your finest turtleneck!' 
-];
-
-var slideDelays = [8000, 5000, 5000, 5000, 5000, 20000];
-var $images = [];
-
-//preload images in a very not DRY way
-$images.push( $('<img>').attr('src', './slideshow/2.jpg').addClass('img-responsive img-rounded') );
-$images.push( $('<img>').attr('src', './slideshow/3.jpg').addClass('img-responsive img-rounded') );
-$images.push( $('<img>').attr('src', './slideshow/4.jpg').addClass('img-responsive img-rounded') );
-$images.push( $('<img>').attr('src', './slideshow/5.jpg').addClass('img-responsive img-rounded') );
-$images.push( $('<img>').attr('src', './slideshow/6.jpg').addClass('img-responsive img-rounded') );
 
 //helps keep the form from holding the page up if you try to leave
 window.onbeforeunload = null; 
 
-$(document).ready( function(){
-
-  //var $slideText = $('.slideshow p').html(messages.shift());
-
-  //var audio = new Audio('joytoworld.mp3');
-  //audio.play();
-
-  // $('#hiddenBody').hide( 0, function(){
-  //   $('.slideshow').fadeIn(3000, function(){
-  //     startSlideShow();
-  //   });
-  // });
-
+$(document).ready(function(){
   openEvite();
-
 });
-
-
-function startSlideShow() {
-
-  var $slideshow = $('.slide');
-  var $slideImg = $('.slideImg');
-  var $slideText = $('.slideshow p');
-  var nextSlide = 2; 
-  var slideSrc = './slideshow/'+ nextSlide + '.jpg';
-  var i;  
-
-  //............Helper Functions........
-  var switchSlide, fadeSlide;
-
-  switchSlide = function() {
-    //slideSrc = './slideshow/'+ nextSlide + '.jpg'; 
-    //$slideImg.attr('src' ,slideSrc);
-    $slideImg.find('img').replaceWith($images.shift());
-    $slideText.html(messages.shift());
-    nextSlide++;
-  };
-  
-  fadeSlide = function($slideShow) {
-    var next = $slideshow
-    .delay(slideDelays.shift())
-    .fadeOut(3000, function(){ switchSlide(); })
-    .fadeIn(3000);
-    return next;
-  };
-
-  //......Execution start here....
-  //...Run the slide show.....
-  for(i = 0; i <= 4; i++) {
-    $slideshow = fadeSlide($slideshow);
-  }
-
-  //...Exit the slide show....
-  $slideshow.delay(slideDelays.shift())
-  .fadeOut(3000, function(){
-    $('.slideshow').hide(0, function(){
-      openEvite();
-    });
-  });
-
-}
-
 
 function openEvite() {
 
   //hide everything bring in the background and bring it all back
-  $('#hiddenBody').hide()
-  .css("background-image", "url(background1.jpg)")
-  .css("background-size", "cover")
-  .css("background-repeat", "no-repeat")
-  .css("background-position", "center center")
+  $('#hidden-body').hide()
+  //.css("background-image", "url(background1.jpg)")
   .fadeIn(3000);
 
   var audio = new Audio('fireplace.mp3');
@@ -135,7 +40,6 @@ function openEvite() {
 
     //hide the form
     $('.rsvp-area').addClass('hidden');
-
    
     //unhide the responses
     //hiding and unhiding used here due to mixed use of
@@ -152,3 +56,11 @@ function openEvite() {
         
   });
 }
+
+
+//preload images in a very not DRY way
+// $images.push( $('<img>').attr('src', './slideshow/2.jpg').addClass('img-responsive img-rounded') );
+// $images.push( $('<img>').attr('src', './slideshow/3.jpg').addClass('img-responsive img-rounded') );
+// $images.push( $('<img>').attr('src', './slideshow/4.jpg').addClass('img-responsive img-rounded') );
+// $images.push( $('<img>').attr('src', './slideshow/5.jpg').addClass('img-responsive img-rounded') );
+// $images.push( $('<img>').attr('src', './slideshow/6.jpg').addClass('img-responsive img-rounded') );

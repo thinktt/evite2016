1845612185c2583ff60f46b8e81d6f834bb46e9a
thinktt <github@thinktt.e4ward.com>
2016-11-24 02:08:38 -0600

    use windows.onload instead of doucment ready

--- evite.js
+++ evite.js
@@ -2,9 +2,13 @@
 //helps keep the form from holding the page up if you try to leave
 window.onbeforeunload = null; 
 
-$(document).ready(function(){
+// $(document).ready(function(){
+//   openEvite();
+// });
+
+$(window).load(function() {function(){
   openEvite();
-});
+}
 
 function openEvite() {
 
